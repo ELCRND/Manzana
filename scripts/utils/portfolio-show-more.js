3c46f502d138f8f1976ab3629b0590954bc7afb1
ELCRND <jwhh2a@gmail.com>
2025-02-28 21:02:17 +0300

    modified:   css/main.css 	modified:   css/main.css.map 	modified:   index.html 	modified:   scripts/utils/portfolio-show-more.js 	modified:   scss/blocks/_banner.scss 	modified:   scss/blocks/_coop.scss 	modified:   scss/blocks/_footer.scss 	modified:   scss/blocks/_integration.scss 	modified:   scss/blocks/_interface.scss 	modified:   scss/blocks/_portfolio.scss 	modified:   scss/blocks/_principles.scss 	modified:   scss/blocks/_tools.scss

--- scripts/utils/portfolio-show-more.js
+++ scripts/utils/portfolio-show-more.js
@@ -1,12 +1,24 @@
 const PORTFOLIO_BUTTON = document.querySelector(".portfolio__btn");
-const HIDDEN_ITEMS = document.querySelectorAll(".portfolio__item.hidden");
+const HIDDEN_ITEMS = document.querySelectorAll(".portfolio__item");
 
 export function portfolioBtnAddEvent() {
+  let currentIndex = window.innerWidth > 767 ? 7 : 3;
   PORTFOLIO_BUTTON.addEventListener("click", function () {
-    HIDDEN_ITEMS.forEach((item) => {
-      item.classList.remove("hidden");
-    });
+    const itemsToShow = 3;
 
-    PORTFOLIO_BUTTON.disabled = true;
+    for (let i = currentIndex; i < currentIndex + itemsToShow; i++) {
+      if (i < HIDDEN_ITEMS.length) {
+        HIDDEN_ITEMS[i].classList.add("visible");
+      } else {
+        PORTFOLIO_BUTTON.disabled = true;
+        break;
+      }
+    }
+
+    currentIndex += itemsToShow;
+
+    if (currentIndex >= HIDDEN_ITEMS.length) {
+      PORTFOLIO_BUTTON.disabled = true;
+    }
   });
 }
